7ea78b23637e56cf092efec9ca93d387af8414be
CH-RAFAY <chrafayather@gmail.com>
2025-07-17 09:33:43 -0700

    deleted

--- JS/mobile-optimizations.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  if (window.innerWidth <= 650) {
-    // Smooth scroll and prevent refresh without interfering touch listeners
-    document.body.style.overscrollBehavior = 'contain';
-    document.documentElement.style.scrollBehavior = 'smooth';
-  }
-});
